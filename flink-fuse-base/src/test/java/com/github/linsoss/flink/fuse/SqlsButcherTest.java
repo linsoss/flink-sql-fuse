2a8c97bbb811ae064b78e26d110763ae0aad8722
Linying Assad <linying@apache.org>
2023-10-20 22:57:47 +0800

    refactor sql scripts splitter

--- flink-fuse-base/src/test/java/com/github/linsoss/flink/fuse/SqlsButcherTest.java
+++ flink-fuse-base/src/test/java/com/github/linsoss/flink/fuse/SqlsButcherTest.java
@@ -10,27 +10,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class SqlsButcherTest {
 
     @Test
-    public void testSplitSqls() throws SqlParseException {
+    public void testSplitSqls1() {
         var sqls = "CREATE TABLE datagen (f1 INT,f2 INT,f4 STRING) WITH ('connector' = 'datagen');\n" +
                 "select * from datagen; " +
                 "insert into hole select * from datagen";
         var expected = new ArrayList<>() {{
-            add("CREATE TABLE `datagen` (\n" +
-                    "  `f1` INTEGER,\n" +
-                    "  `f2` INTEGER,\n" +
-                    "  `f4` STRING\n" +
-                    ") WITH (\n" +
-                    "  'connector' = 'datagen'\n" +
-                    ")");
-            add("SELECT *\n" +
-                    "FROM `datagen`");
-            add("INSERT INTO `hole`\n" +
-                    "(SELECT *\n" +
-                    "FROM `datagen`)");
+            add("CREATE TABLE datagen (f1 INT,f2 INT,f4 STRING) WITH ('connector' = 'datagen')");
+            add("select * from datagen");
+            add("insert into hole select * from datagen");
         }};
-        var results = SqlsButcher.astSplit(sqls);
+        var results = SqlsButcher.splitSqls(sqls);
         assertEquals(expected, results);
     }
+
+    @Test
+    public void testSplitSqls2()  {
+        var sqls = "CREATE TABLE datagen (f1 INT,f2 INT,f4 STRING) WITH ('connector' = 'datagen');\n" +
+                "select replace(';', '_') from datagen; " +
+                "insert into hole select replace(\";\", \"_\")  from datagen";
+        var expected = new ArrayList<>() {{
+            add("CREATE TABLE datagen (f1 INT,f2 INT,f4 STRING) WITH ('connector' = 'datagen')");
+            add("select replace(';', '_') from datagen");
+            add("insert into hole select replace(\";\", \"_\")  from datagen");
+        }};
+        var results = SqlsButcher.splitSqls(sqls);
+        assertEquals(expected, results);
+    }
+
 
 }
 
